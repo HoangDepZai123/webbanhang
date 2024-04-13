72edb353304ad1217c10f5fc5e2886f4731638fb
trandinhtho <87253707+trandinhtho@users.noreply.github.com>
2024-04-14 00:00:32 +0700

    Tho 2

---
+++ hello.php
@@ -0,0 +1,20 @@
+<?php
+
+// Đây là một ví dụ về một đoạn mã PHP đơn giản
+
+// Khai báo một biến và gán giá trị cho nó
+$message = "Xin chào, đây là một đoạn mã PHP đơn giản!";
+
+// Hiển thị nội dung của biến ra màn hình
+echo $message;
+
+// Định nghĩa một hàm đơn giản
+function sayHello($name) {
+    echo "Xin chào, " . $name . "!";
+}
+
+// Gọi hàm sayHello với đối số là "Người dùng"
+sayHello("Người dùng");
+
+?>
+gi
