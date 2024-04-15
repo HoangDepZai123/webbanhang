d28b668abd9388d2365b46b80751dd93655b6a60
trandinhtho <87253707+trandinhtho@users.noreply.github.com>
2024-04-15 23:00:43 +0700

    add chuyen  nhanh nha

---
+++ dinhtho.js
@@ -0,0 +1,15 @@
+// Đây là một ví dụ về một tập tin JavaScript đơn giản
+
+// Khai báo một biến
+let message = "Xin chào, đây là dinhtho.js";
+
+// Hiển thị nội dung của biến message ra console
+console.log(message);
+
+// Định nghĩa một hàm đơn giản
+function sayHello(name) {
+    console.log("Xin chào, " + name + "!");
+}
+
+// Gọi hàm sayHello với đối số là "Người dùng"
+sayHello("Người dùng");
